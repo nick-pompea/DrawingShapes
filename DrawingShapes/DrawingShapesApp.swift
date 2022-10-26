fa27d6200809e7dfd8fba225fac353ac85464d19
Nicholas Pompea <nick.pompea@gmail,com>
2022-10-26 11:43:58 -0600

    Initial Commit

---
+++ DrawingShapes/DrawingShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingShapesApp.swift
+//  DrawingShapes
+//
+//  Created by Nicholas Pompea on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
